cb11cbc976798f986e391b1e7fd6725c7616a002
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-11-11 20:56:29 -0300

    Criado componente para a galeria.

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,24 +1,85 @@
 import Section from '../Section'
-import zelda from '../../assets/images/zelda.png'
-import { ListItens, Itens } from './styles'
+import { ListItens, Itens, Action, Modal, ModalContent } from './styles'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <ListItens>
-      <Itens>
-        <img src={zelda} alt="imagem do link" />
-      </Itens>
-      <Itens>
-        <img src={zelda} alt="imagem do link" />
-      </Itens>
-      <Itens>
-        <img src={zelda} alt="imagem do link" />
-      </Itens>
-      <Itens>
-        <img src={zelda} alt="imagem do link" />
-      </Itens>
-    </ListItens>
-  </Section>
-)
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import spiderMan from '../../assets/images/banner-homem-aranha.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'image',
+    url: starWars
+  },
+  {
+    type: 'image',
+    url: spiderMan
+  },
+  {
+    type: 'video',
+    url: 'http://www.youtube.com/embed/uHGSqcAHlQ'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <ListItens>
+          {mock.map((media, index) => (
+            <Itens key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${name} `}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia."
+                />
+              </Action>
+            </Itens>
+          ))}
+        </ListItens>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} alt="Ícone de fechar" />
+          </header>
+          <img src={spiderMan} alt="" />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
 
 export default Gallery
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -3,6 +3,8 @@ import Hero from '../../components/Hero'
 import Section from '../../components/Section'
 import Gallery from '../../components/Gallery'
 
+import residentEvil from '../../assets/images/resident.png'
+
 const Product = () => {
   const { id } = useParams()
 
@@ -35,7 +37,7 @@ const Product = () => {
           jogo.
         </p>
       </Section>
-      <Gallery />
+      <Gallery defaultCover={residentEvil} name="Jogo Teste" />
     </>
   )
 }
